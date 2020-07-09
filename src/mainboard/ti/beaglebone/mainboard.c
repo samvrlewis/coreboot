f4ac18a7daef9a5bd154b731034b37de238f8f0f
Sam Lewis <sam.vr.lewis@gmail.com>
2020-07-09 19:32:50 +1000

    Add boot mem properly

---
+++ src/mainboard/ti/beaglebone/mainboard.c
@@ -0,0 +1,14 @@
+#include <symbols.h>
+#include <bootmem.h>
+#include <console/console.h>
+#include <device/device.h>
+
+static void mainboard_enable(struct device *dev)
+{
+	printk(BIOS_DEBUG, "MAINBOARD ENABLE\n");
+	ram_resource(dev, 0, (uintptr_t)_dram / KiB, 512 * KiB);
+}
+
+struct chip_operations mainboard_ops = {
+	.enable_dev = mainboard_enable,
+};
