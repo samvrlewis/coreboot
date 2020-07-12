771c0b1f921c2cf77f7eb51a48d9831ff3138c19
Sam Lewis <sam.vr.lewis@gmail.com>
2020-07-12 18:22:02 +1000

    set ram resource from config

--- src/mainboard/ti/beaglebone/mainboard.c
+++ src/mainboard/ti/beaglebone/mainboard.c
@@ -6,7 +6,7 @@
 static void mainboard_enable(struct device *dev)
 {
 	printk(BIOS_DEBUG, "MAINBOARD ENABLE\n");
-	ram_resource(dev, 0, (uintptr_t)_dram / KiB, 512 * KiB);
+	ram_resource(dev, 0, (uintptr_t)_dram / KiB, CONFIG_DRAM_SIZE_MB * MiB/KiB);
 }
 
 struct chip_operations mainboard_ops = {
