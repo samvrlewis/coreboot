515ec061cb9de570ae4e6f524b1a0fd9a869bbb7
Sam Lewis <sam.vr.lewis@gmail.com>
2020-08-06 21:13:22 +1000

    mb/ti/beaglebone: Initialize DDR3
    Adds initialisation of 512MB of DDR memory on the BBB to the romstage.
    The parameters for the DDR peripherals are taken from U-Boot.
    
    Change-Id: I692bfd913c8217a78d073d19c5344c9bb40722a8
    Signed-off-by: Sam Lewis <sam.vr.lewis@gmail.com>

--- src/mainboard/ti/beaglebone-black/romstage.c
+++ src/mainboard/ti/beaglebone-black/romstage.c
@@ -2,11 +2,20 @@
 
 #include <program_loading.h>
 #include <console/console.h>
+#include <cbmem.h>
+
+#include <soc/ti/am335x/sdram.h>
+#include "ddr3.h"
 
 void main(void)
 {
 	console_init();
 	printk(BIOS_INFO, "Hello from romstage.\n");
+
+	config_ddr(400, &ioregs_bonelt, &ddr3_beagleblack_data, &ddr3_beagleblack_cmd_ctrl_data,
+		   &ddr3_beagleblack_emif_reg_data, 0);
+
+	cbmem_initialize_empty();
 
 	run_ramstage();
 }
